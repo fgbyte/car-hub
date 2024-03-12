7eea4b361b1924ecf009b7606e6c11e8424750f0
fgbyte <fer.garciabayon@gmail.com>
2024-03-12 14:28:26 -0500

    working on footer

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,12 @@
 //este archivo es para importar todos los componentes mas fáciles
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export {
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar,
+    Footer
 }
